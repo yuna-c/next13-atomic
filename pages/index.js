d6af5b294507ae993c69f2bde56ef0ee7338b849
yuna <yuna744@gmail.com>
2024-01-22 15:49:25 +0900

    0122 isr방식으로 메인 페이지 컴포넌트에 서버데이터 활용해서 prerender페이지 생성

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head';
+import Head from "next/head";
+import axios from "axios";
 
-export default function Home() {
+export default function Home(props) {
+  console.log("ssg", props);
   return (
     <>
       <Head>
@@ -12,4 +14,14 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+//ssg방식으로 페이지 렌더링
+export async function getStaticProps() {
+  //serialize 에러시 반환받은 propmise객체에서 실제 data값까지 뽑아낸다음에 props로 전달
+  const { data } = await axios.get(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+  );
+  //리턴으로 props객체를 컴포넌트로 전달시 revalidate가 없으면 ssg방식, 있으면 isr방식
+  return { props: data, revalidate: 60 * 60 * 24 };
 }
