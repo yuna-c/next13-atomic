558c4722ac0cfcd0192d5891cdd5fbd63a474875
yuna <yuna744@gmail.com>
2024-01-22 16:51:43 +0900

    0122 imgTxt props에 따라 Pic박스 구조 변경

--- components/pic/Pic.jsx
+++ components/pic/Pic.jsx
@@ -2,9 +2,10 @@ import clsx from "clsx";
 import styles from "./pic.module.scss";
 import Image from "next/image";
 
-export default function Pic({ imgSrc }) {
+export default function Pic({ imgSrc, imgTxt }) {
+  console.log(imgTxt);
   return (
-    <div className={clsx(styles.pic)}>
+    <div className={clsx(styles.pic, imgTxt && styles.picTxt)}>
       <Image
         src={imgSrc}
         alt={imgSrc}
@@ -12,6 +13,7 @@ export default function Pic({ imgSrc }) {
         fill
         sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw"
       />
+      {imgTxt && <h2>{imgTxt}</h2>}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import clsx from "clsx";
 export default function Home({ meals }) {
   console.log("ssg", meals);
   const mealsData = meals.slice(0, 5);
-
   return (
     <>
       <Head>
@@ -17,14 +16,24 @@ export default function Home({ meals }) {
 
       <main className={styles.main}>
         <h1>Main page</h1>
-        {mealsData.map((item) => {
-          return (
-            <div key={item.idMeal}>
-              <div className={clsx(styles.bg)}>
-                <Pic imgSrc={item.strMealThumb} />
+        {mealsData.map((item, idx) => {
+          if (0 === idx % 2) {
+            return (
+              <div key={item.idMeal}>
+                <div className={clsx(styles.bg)}>
+                  <Pic imgSrc={item.strMealThumb} />
+                </div>
               </div>
-            </div>
-          );
+            );
+          } else {
+            return (
+              <div key={item.idMeal}>
+                <div className={clsx(styles.bg)}>
+                  <Pic imgSrc={item.strMealThumb} imgTxt={item.strMeal} />
+                </div>
+              </div>
+            );
+          }
         })}
       </main>
     </>
@@ -33,8 +42,6 @@ export default function Home({ meals }) {
 
 //ssg방식으로 페이지 렌더링
 export async function getStaticProps() {
-  //serialize 에러시 반환받은 propmise객체에서 실제 data값까지 뽑아낸다음에 props로 전달
   const { data } = await axios.get("/filter.php?c=Seafood");
-  //리턴으로 props객체를 컴포넌트로 전달시 revalidate가 없으면 ssg방식, 있으면 isr방식
   return { props: data, revalidate: 60 * 60 * 24 };
 }
