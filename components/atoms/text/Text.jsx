503cc52eed07f866b18e534fa8bc12c69627af83
yuna <yuna744@gmail.com>
2024-01-23 17:58:02 +0900

    0123 onClick핸들러에 전달되는 값들을 원소단위 Text에서 비구조할당으로 필수항목 연결

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -1,43 +1,34 @@
-import clsx from "clsx";
-import styles from "./text.module.scss";
-import React from "react";
-import Link from "next/link";
-import { Nanum_Myeongjo, Orbitron } from "next/font/google";
+import clsx from 'clsx';
+import styles from './text.module.scss';
+import React from 'react';
+import Link from 'next/link';
+import { Nanum_Myeongjo, Orbitron } from 'next/font/google';
 
 const nanum = Nanum_Myeongjo({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  preload: true,
-  variable: "--font-nanum",
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	preload: true,
+	variable: '--font-nanum'
 });
 
 const orbitron = Orbitron({
-  subsets: ["latin"],
-  weight: ["400", "600", "900"],
-  preload: true,
-  variable: "--font-orbitron",
+	subsets: ['latin'],
+	weight: ['400', '600'],
+	preload: true,
+	variable: '--font-orbitron'
 });
 
-export default function Text({
-  children,
-  url,
-  tagName = "p",
-  styleType,
-  className,
-  isOn = false,
-}) {
-  return React.createElement(
-    tagName,
-    {
-      className: clsx(
-        styles.text,
-        nanum.variable,
-        orbitron.variable,
-        styles[styleType],
-        className,
-        isOn && styles.on
-      ),
-    },
-    url ? React.createElement(Link, { href: url }, children) : children
-  );
+//Text컴포넌트 입장에서 부모로부터 이벤트핸들러 전달시 (순서, fetchingData, e)
+//위의 값3가지를 비구조할당을 전달 (없으면 그만, 있으면 활용)
+export default function Text({ children, url, tagName = 'p', styleType, className, isOn = false, onClick, data, idx }) {
+	return React.createElement(
+		tagName,
+		{
+			className: clsx(styles.text, nanum.variable, orbitron.variable, styles[styleType], className, isOn && styles.on),
+			//onClick props로 전달되는 핸들러함수에 어떤 값이 쓰일지 모르니 일단 다 연결
+			//주의점: 객체로 인수를 묶는 이유- 특정 인수가 전달되지 않았을때 파라미터 순서가 변경되는 걸 방지하기 위함
+			onClick: e => onClick({ e, idx, data })
+		},
+		url ? React.createElement(Link, { href: url }, children) : children
+	);
 }
--- pages/gallery/index.jsx
+++ pages/gallery/index.jsx
@@ -1,12 +1,23 @@
-import clsx from "clsx";
-import styles from "./gallery.module.scss";
-import Navbar from "@/components/molecules/navbar/Navbar";
-
+import clsx from 'clsx';
+import styles from './gallery.module.scss';
+import Navbar from '@/components/molecules/navbar/Navbar';
+import Category from '@/components/molecules/category/Category';
 export default function Gallery() {
-  return (
-    <section className={clsx(styles.gallery)}>
-      <h1>Gallery Page</h1>
-      <Navbar data={["Find Recipe", "Gallery", "About"]} />
-    </section>
-  );
+	const fetchData = [
+		{ name: 'David', age: 20 },
+		{ name: 'Emily', age: 30 },
+		{ name: 'Paul', age: 40 }
+	];
+	const nameArr = fetchData.map(data => data.name);
+
+	const handleClick = ({ data, idx }) => {
+		console.log(data[idx]);
+	};
+	return (
+		<section className={clsx(styles.gallery)}>
+			<h1>Gallery Page</h1>
+			<Navbar data={['Find Recipe', 'Gallery', 'About']} />
+			<Category fetchDataArr={fetchData} nameArr={nameArr} onClick={handleClick} />
+		</section>
+	);
 }
