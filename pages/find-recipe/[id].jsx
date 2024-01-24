097c8fe9dc4216c514f68f0f61e73878b6a1f7e1
yuna <yuna744@gmail.com>
2024-01-24 18:13:25 +0900

    0124 상세페이지 데이터 출력 및 스타일링

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -1,9 +1,28 @@
-import Breadcrumb from "@/components/molecules/breadcrumb/Breadcrumb";
+import Pic from '@/components/atoms/pic/Pic';
+import { useRecipeById } from '@/hooks/useRecipe';
+import { useRouter } from 'next/router';
+import styles from './detail.module.scss';
+import clsx from 'clsx';
+import Breadcrumb from '@/components/molecules/breadcrumb/Breadcrumb';
 
 export default function Detail() {
-  return (
-    <section>
-      <Breadcrumb divider={">"} />
-    </section>
-  );
+	const router = useRouter();
+	const { id } = router.query;
+	const { data, isSuccess } = useRecipeById(id);
+	console.log(data);
+	console.log(router.query);
+
+	return (
+		<section className={clsx(styles.detail)}>
+			{/* <Breadcrumb divider={'>'} /> */}
+			{isSuccess && (
+				<>
+					<h1>{data.strMeal}</h1>
+					<div className={clsx(styles.picFrame)}>
+						<Pic imgSrc={data.strMealThumb} />
+					</div>
+				</>
+			)}
+		</section>
+	);
 }
