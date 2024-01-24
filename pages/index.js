bfa1e4078f5801136000d262cdf1a7706e403c96
yuna <yuna744@gmail.com>
2024-01-24 10:12:32 +0900

    0124 Category 컴포넌트 동작 흐름

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,34 @@
-import Head from "next/head";
-import axios from "axios";
-import Image from "next/image";
-import Pic from "@/components/atoms/pic/Pic";
-import clsx from "clsx";
-import Text from "@/components/atoms/text/Text";
-import styles from "./Home.module.scss";
-import List from "@/components/atoms/list/List";
-import Input from "@/components/atoms/input/Input";
-import { useState } from "react";
+import Head from 'next/head';
+import axios from 'axios';
+import Image from 'next/image';
+import Pic from '@/components/atoms/pic/Pic';
+import clsx from 'clsx';
+import Text from '@/components/atoms/text/Text';
+import styles from './Home.module.scss';
+import List from '@/components/atoms/list/List';
+import Input from '@/components/atoms/input/Input';
+import { useState } from 'react';
 
 export default function Home({ meals }) {
-  console.log("ssg", meals);
-  const mealsData = meals.slice(0, 5);
-  const topRated = ["Avartar", "Emma", "AquaMan"];
-  const url = ["/", "/gallery", "/about"];
-  const [Val, setVal] = useState("");
-  console.log(Val);
+	console.log('ssg', meals);
+	const mealsData = meals.slice(0, 5);
+	const topRated = ['Avartar', 'Emma', 'AquaMan'];
+	const url = ['/', '/gallery', '/about'];
+	const [Val, setVal] = useState('');
+	console.log(Val);
 
-  return (
-    <>
-      <Head>
-        <title>Main Page</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Main Page</title>
+			</Head>
 
-      <main className={styles.main}></main>
-    </>
-  );
+			<main className={styles.main}>이게모야?</main>
+		</>
+	);
 }
 
 export async function getStaticProps() {
-  const { data } = await axios.get("/filter.php?c=Seafood");
-  return { props: data, revalidate: 60 * 60 * 24 };
+	const { data } = await axios.get('/filter.php?c=Seafood');
+	return { props: data, revalidate: 60 * 60 * 24 };
 }
