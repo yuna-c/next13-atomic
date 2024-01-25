3ad4af707e311d9137c34385ba022a50c764417d
yuna <yuna744@gmail.com>
2024-01-25 16:00:32 +0900

    0125 즐겨찾기한 항목 My Favoraite페이지에서 목록으로 출력

--- pages/my-favorait/index.jsx
+++ pages/my-favorait/index.jsx
@@ -1,13 +1,37 @@
 import clsx from 'clsx';
 import styles from './my-favorait.module.scss';
+import { useEffect, useState } from 'react';
+import { useRecipesByIds } from '@/hooks/useRecipe';
+import Card from '@/components/molecules/card/Card';
 
 export default function MyFavoraite() {
-	const queryArr = ['a', 'b', 'c'];
-	//const resultArr = queryArr.map(el => useRecipeById(el));
+	const [SavedId, setSavedId] = useState([]);
+
+	useEffect(() => {
+		setSavedId(JSON.parse(localStorage.getItem('favoraite')) || []);
+	}, []);
+
+	const result = useRecipesByIds(SavedId);
+	console.log(result);
 
 	return (
 		<section className={clsx(styles.myFavorait)}>
 			<h1>My Favorait</h1>
+
+			{result.map(({ data, isSuccess }) => {
+				if (isSuccess) {
+					return (
+						<Card
+							key={data.idMeal}
+							imgSrc={data.strMealThumb}
+							txt={data.strMeal}
+							className={styles.favoraitCard}
+							url={`/find-recipe/${data.idMeal}?name=${data.strMeal}`}
+							// ?name 쿼리스트링 방식
+						/>
+					);
+				}
+			})}
 		</section>
 	);
 }
