2f10127fdff6b06a33976cde921bd669b54cc595
yuna <yuna744@gmail.com>
2024-01-25 15:38:46 +0900

    0125 동일한 fetching함수로 여러개의 useQuery의 리턴값을 받을때 useQuery훅을 반복호출하는게 아닌 useQueies를 활용해야 되는 이유 고민

--- pages/my-favorait/index.jsx
+++ pages/my-favorait/index.jsx
@@ -2,6 +2,9 @@ import clsx from 'clsx';
 import styles from './my-favorait.module.scss';
 
 export default function MyFavoraite() {
+	const queryArr = ['a', 'b', 'c'];
+	//const resultArr = queryArr.map(el => useRecipeById(el));
+
 	return (
 		<section className={clsx(styles.myFavorait)}>
 			<h1>My Favorait</h1>
